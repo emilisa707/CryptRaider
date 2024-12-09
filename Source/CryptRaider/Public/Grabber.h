44fda64502c233b52b4dc7a6e629f69dd4bca166
Emilisa <emilisa90@gmail.com>
2024-12-09 23:44:18 +0700

    90. geometry sweeping

---
+++ Source/CryptRaider/Public/Grabber.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class CRYPTRAIDER_API UGrabber : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+private:	
+	
+	UPROPERTY(EditAnywhere)
+	float MaxGrabDistance = 10.f;
+
+	UPROPERTY(EditAnywhere)
+	float GrabRadius = 100.f;
+};
